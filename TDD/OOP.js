3e28bc82fa5c27d82e4abe0b14abdaf67e0f5325
jeong <7863z@naver.com>
2021-12-14 16:15:22 +0900

    TDD, Algorithm

--- TDD/OOP.js
+++ TDD/OOP.js
@@ -1,8 +1,8 @@
 // 객체지향
 
-// 1. 모듈 패턴
+// 1. 모듈 패턴 (값을 은닉하기위해 사용)
 function Person() {
-  let age = 35; // 값을 은닉
+  let age = 15; // 값을 은닉
   
   return {
     getAge: function(){ return age },
@@ -13,9 +13,10 @@ function Person() {
 const person = Person();
 console.log(person.getAge());
 
-// 2. 사용자 정의 타입 패턴 (생성자 함수)
+
+// 2. 사용자 정의 타입 패턴 (생성자 함수), 메모리 효율적
 function PersonType() {
-  this.age = 35;
+  this.age = 25;
 }
 
 PersonType.prototype.getAge = function() {
@@ -25,3 +26,37 @@ PersonType.prototype.getAge = function() {
 const instancePerson = new PersonType();
 
 console.log(instancePerson.getAge());
+
+
+// 3. 모듈 + 사용자 정의 타입
+// function PersonType2() {
+//   let age = 35;
+  
+//   function innerPersonType() { }
+
+//   innerPersonType.prototype.getAge = function () {
+//     return age;
+//   }
+
+//   return innerPersonType;
+// }
+
+// const Person3 = PersonType2();
+// const person3 = new Person3();
+// console.log(person3.getAge());
+
+// IIFE 패턴으로 만들어보기
+const PersonType3 = (function PersonType2() {
+  let age = 45;
+  
+  function innerPersonType() { }
+
+  innerPersonType.prototype.getAge = function () {
+    return age;
+  }
+
+  return innerPersonType;
+})();
+
+const personType3 = new PersonType3(); 
+console.log(personType3.getAge());
